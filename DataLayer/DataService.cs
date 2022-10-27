58820dc9451cb005b31b8c942364983d445549c3
Henrik Bulskov <bulskov@ruc.dk>
2022-10-28 01:41:21 +0200

    Add project files.

---
+++ DataLayer/DataService.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+
+namespace DataLayer
+{
+    public class DataService : IDataService
+    {
+        private readonly List<Category> _categories = new List<Category>
+        {
+            new Category{ Id = 1, Name = "Beverages", Description = "Soft drinks, coffees, teas, beers, and ales"},
+            new Category{ Id = 2, Name = "Condiments", Description = "Sweet and savory sauces, relishes, spreads, and seasonings"},
+            new Category{ Id = 3, Name = "Confections", Description = "Desserts, candies, and sweet breads"},
+            new Category{ Id = 4, Name = "Dairy Products", Description = "Cheeses"},
+            new Category{ Id = 5, Name = "Grains/Cereals", Description = "Breads, crackers, pasta, and cereal"},
+            new Category{ Id = 6, Name = "Meat/Poultry", Description = "Prepared meats"},
+            new Category{ Id = 7, Name = "Produce", Description = "Dried fruit and bean curd"},
+            new Category{ Id = 8, Name = "Seafood", Description = "Seaweed and fish"}
+        };
+
+        private readonly List<Product> _products = new List<Product>();
+
+
+        public DataService()
+        {
+            _products.Add(new Product { Id = 1, Name = "Chai", Category = GetCategory(1) });
+            _products.Add(new Product { Id = 2, Name = "Chang", Category = GetCategory(1) });
+            _products.Add(new Product { Id = 3, Name = "Aniseed Syrup", Category = GetCategory(2) });
+            _products.Add(new Product { Id = 4, Name = "Chef Anton's Cajun Seasoning", Category = GetCategory(2) });
+        }
+
+
+        public IList<Category> GetCategories()
+        {
+            return _categories;
+        }
+
+        public Category? GetCategory(int id)
+        {
+            return _categories.FirstOrDefault(x => x.Id == id);
+        }
+
+        public void CreateCategory(Category category)
+        {
+            var maxId = _categories.Max(x => x.Id);
+            category.Id = maxId + 1;
+            _categories.Add(category);
+        }
+
+        public bool UpdateCategory(Category category)
+        { 
+            var dbCat = GetCategory(category.Id);
+            if (dbCat == null)
+            {
+                return false;
+            }
+            dbCat.Name = category.Name;
+            dbCat.Description = category.Description;
+            return true;
+        }
+
+        public bool DeleteCategory(int id)
+        {
+            var dbCat = GetCategory(id);
+            if (dbCat == null)
+            {
+                return false;
+            }
+            _categories.Remove(dbCat);
+            return true;
+        }
+
+
+        public IList<Product> GetProducts()
+        {
+            return _products;
+        }
+
+        public Product? GetProduct(int id)
+        {
+            return _products.FirstOrDefault(x => x.Id == id);
+        }
+    }
+}
---
+++ DataLayer/IDataService.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace DataLayer
+{
+    public interface IDataService
+    {
+        IList<Category> GetCategories();
+        Category? GetCategory(int id);
+        IList<Product> GetProducts();
+        Product? GetProduct(int id);
+        void CreateCategory(Category category);
+        bool UpdateCategory(Category category);
+        bool DeleteCategory(int id);
+    }
+}
