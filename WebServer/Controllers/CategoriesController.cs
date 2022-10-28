00e5fe3030193d8297b0a09b2bc23a094fe72a45
bulskov <bulskov@ruc.dk>
2022-10-28 11:16:27 +0200

    GetCategory

--- WebServer/Controllers/CategoriesController.cs
+++ WebServer/Controllers/CategoriesController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using DataLayer;
 using Microsoft.AspNetCore.Mvc;
 
 namespace WebServer.Controllers
@@ -10,10 +6,27 @@ namespace WebServer.Controllers
     [Route("api/categories")]
     public class CategoriesController : ControllerBase
     {
+        private IDataService _dataService = new DataService();
+
         [HttpGet]
-        public string Get()
+        public IActionResult Get()
         {
-            return "Hello from controller";
+            var categories = _dataService.GetCategories();
+            return Ok(categories);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetCategory(int id)
+        {
+            var category = _dataService.GetCategory(id);
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(category);
+
         }
     }
 }
