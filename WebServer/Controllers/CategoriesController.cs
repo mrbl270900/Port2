97864fafea3248aba56f3ee537546ff962abecb9
bulskov <bulskov@ruc.dk>
2022-10-28 10:52:01 +0200

    init asp

---
+++ WebServer/Controllers/CategoriesController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebServer.Controllers
+{
+    [Route("api/categories")]
+    public class CategoriesController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Hello from controller";
+        }
+    }
+}
