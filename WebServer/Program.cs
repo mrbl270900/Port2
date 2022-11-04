0f8e5388a2f540a47179ed4f87364df45dc9cc5f
Unhunghero94 <fredriklund@outlook.dk>
2022-11-04 12:49:32 +0100

    Jeg har kommenteret i program.cs

--- WebServer/Program.cs
+++ WebServer/Program.cs
@@ -4,6 +4,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
+// jeg har lavet en kommentar i program.cs
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 builder.Services.AddSingleton<IDataService, DataService>();
