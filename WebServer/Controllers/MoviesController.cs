b5e1a5088533c2915e3121adc0eb56491fff66f9
Simon Prip Cardien Vinther <svinther@hotmail.com>
2022-11-16 20:11:05 +0100

    Adding moviecontroller

---
+++ WebServer/Controllers/MoviesController.cs
@@ -0,0 +1,40 @@
+﻿using DataLayer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using WebServer.Models;
+using WebServiceToken.Models;
+
+namespace WebServiceSimple.Controllers
+{
+    [ApiController]
+    [Route("api/movies")]
+
+    public class MoviesController : ControllerBase
+    {
+        private readonly IMovieDataService _moviedataService;
+        private readonly LinkGenerator _generator;
+        private readonly autoMapper _mapper;
+
+        public MoviesController(IMovieDataService moviedataService, LinkGenerator generator, autoMapper _mapper)
+        {
+            _moviedataService = moviedataService;
+            _generator = generator;
+        }
+
+        [HttpGet]
+        [Authorize] //this shows where I have to get Authorization
+        public IActionResult GetMovieTitle(string id)
+        {
+            try
+            {
+                var movies = _moviedataService.GetMovieTitle();
+                return Ok();
+            }
+            catch
+            {
+                return Unauthorized();
+            }
+        }
+
+    }
+}
