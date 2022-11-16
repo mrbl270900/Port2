89e2c91aed01f8bb666d2530a5e21e09e845733b
Mads <mrbl@ruc.dk>
2022-11-16 12:26:39 +0100

    fix generel

---
+++ TestProjekt/WebServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using DataLayer.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using DataLayer;
+
+namespace TestProjekt
+{
+    public class WebServiceTest
+    {
+            [Fact]
+            public void Test1()
+            {
+
+            }
+    }
+}
